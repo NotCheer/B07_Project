16c56bd53bdc539e4648ba9338683c0b43126836
NotCheer <nut.li@mail.utoronto.ca>
2023-11-14 16:45:20 -0500

    update sdk setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.b07_project"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.b07_project"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
