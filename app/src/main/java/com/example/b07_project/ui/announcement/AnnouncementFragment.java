3e3f185430287c910175e749bbf5cf0751a6af4f
NotCheer <nut.li@mail.utoronto.ca>
2023-11-21 11:55:20 -0500

    refactor AnnouncementFragment: file name changed minor typo fixed

--- app/src/main/java/com/example/b07_project/ui/announcement/AnnouncementFragment.java
+++ app/src/main/java/com/example/b07_project/ui/announcement/AnnouncementFragment.java
@@ -12,20 +12,20 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.b07_project.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+public class AnnouncementFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        AnnouncementViewModel announcementViewModel =
+                new ViewModelProvider(this).get(AnnouncementViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        announcementViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/b07_project/ui/announcement/AnnouncementViewModel.java
+++ app/src/main/java/com/example/b07_project/ui/announcement/AnnouncementViewModel.java
@@ -4,13 +4,13 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class AnnouncementViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public AnnouncementViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is Announcement fragment");
     }
 
     public LiveData<String> getText() {
