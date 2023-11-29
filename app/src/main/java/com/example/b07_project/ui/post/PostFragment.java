ea0a4c10d0640042927ba354cdf32a9505d8c17e
MegumiKat <2243997366@qq.com>
2023-11-29 16:08:54 -0500

    admin post

--- app/src/main/java/com/example/b07_project/ui/post/PostFragment.java
+++ app/src/main/java/com/example/b07_project/ui/post/PostFragment.java
@@ -4,37 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.b07_project.AnnouncementModule.Announcement;
 import com.example.b07_project.R;
-import com.example.b07_project.ui.schedule.Event;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.example.b07_project.databinding.FragmentPostBinding;
 import com.google.firebase.database.ValueEventListener;
-
-
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-
-import androidx.fragment.app.Fragment;
 
 public class PostFragment extends Fragment {
 
@@ -52,7 +37,7 @@ public class PostFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_post, container, false);
+        View view = inflater.inflate(R.layout.fragment_postann, container, false);
 
         // Initialize views
         editTextTitle = view.findViewById(R.id.text_title);
