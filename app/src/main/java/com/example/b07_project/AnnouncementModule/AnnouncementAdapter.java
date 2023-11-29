375ad39c855f5b32e7f5dd114293da93f3525cc7
NotCheer <nut.li@mail.utoronto.ca>
2023-11-29 12:40:38 -0500

    implement AnnouncementActivity.java for announcement detail

--- app/src/main/java/com/example/b07_project/AnnouncementModule/AnnouncementAdapter.java
+++ app/src/main/java/com/example/b07_project/AnnouncementModule/AnnouncementAdapter.java
@@ -1,6 +1,10 @@
 package com.example.b07_project.AnnouncementModule;
 
+import android.app.MediaRouteButton;
 import android.content.Context;
+import android.content.Intent;
+import android.transition.TransitionManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +13,16 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.b07_project.MainActivity;
 import com.example.b07_project.R;
+import com.example.b07_project.ui.announcement.AnnouncementActivity;
+import com.example.b07_project.ui.announcement.AnnouncementFragment;
 
 import java.util.List;
 public class AnnouncementAdapter extends RecyclerView.Adapter<AnnouncementAdapter.AnnouncementViewHolder> {
     private List<Announcement> announcementList;
     private LayoutInflater mInflater;
+
 
     public void setAnnouncements(List<Announcement> announcementList) {
         this.announcementList = announcementList;
@@ -24,19 +32,18 @@ public class AnnouncementAdapter extends RecyclerView.Adapter<AnnouncementAdapte
         private final TextView contentTextView;
         private final TextView titleTextView;
         private final TextView dateTextView;
+        public MediaRouteButton details;
+        private final Context context;
 
         public AnnouncementViewHolder(@NonNull View itemView) {
             super(itemView);
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+            context = itemView.getContext();
 
-                }
-            });
             contentTextView = itemView.findViewById(R.id.lineAnnouncementContent);
             titleTextView = itemView.findViewById(R.id.lineAnnouncementTitle);
             dateTextView = itemView.findViewById(R.id.lineAnnouncementDate);
         }
+
 
         public TextView getContentTextView() {
             return contentTextView;
@@ -62,13 +69,25 @@ public class AnnouncementAdapter extends RecyclerView.Adapter<AnnouncementAdapte
         return new AnnouncementViewHolder(view);
 
     }
-
+    private int mExpandedPosition = -1;
+    private RecyclerView recyclerView;
     @Override
     public void onBindViewHolder(@NonNull AnnouncementViewHolder holder, final int position) {
         Announcement a = announcementList.get(position);
         holder.getTitleTextView().setText(a.getTitle());
         holder.getContentTextView().setText(a.getContent());
         holder.getDateTextView().setText(a.getDate());
+
+        holder.getContentTextView().setOnClickListener(new View.OnClickListener() {
+            TextView itemView = holder.getContentTextView();
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(itemView.getContext(), AnnouncementActivity.class);
+                Log.d("announcements","clicked"+Integer.toString(holder.getBindingAdapterPosition()));
+                intent.putExtra("content", announcementList.get(holder.getBindingAdapterPosition()).getContent());
+                itemView.getContext().startActivity(intent);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/b07_project/ui/announcement/AnnouncementActivity.java
+++ app/src/main/java/com/example/b07_project/ui/announcement/AnnouncementActivity.java
@@ -1,6 +1,10 @@
 package com.example.b07_project.ui.announcement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
@@ -9,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.b07_project.AnnouncementModule.Announcement;
 import com.example.b07_project.AnnouncementModule.AnnouncementAdapter;
+import com.example.b07_project.MainActivity;
 import com.example.b07_project.R;
 
 import java.util.ArrayList;
@@ -17,29 +22,27 @@ import java.util.Date;
 public class AnnouncementActivity extends AppCompatActivity {
 
     AnnouncementAdapter adapter;
+    Button btn;
+    TextView text;
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_announcement_display);
+        btn = findViewById(R.id.back_bottom);
+        text = findViewById(R.id.detail_content);
+        Intent intent = getIntent();
 
-        if (savedInstanceState == null) {
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            AnnouncementFragment fragment = new AnnouncementFragment();
-            transaction.replace(R.id.sample_content_fragment, fragment);
-            transaction.commit();
-        }
-        /*
-        // data to populate the RecyclerView with
-        ArrayList<Announcement> announcements = new ArrayList<>();
-        Announcement announcement = new Announcement("aaa","bbb","2003/12/4)");
-        announcements.add(announcement);
 
-        // set up the RecyclerView
-        RecyclerView recyclerView = findViewById(R.id.list_announcement);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new AnnouncementAdapter(announcements);
-        recyclerView.setAdapter(adapter);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AnnouncementActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
-         */
+        text.setText(intent.getExtras().getString("content"));
     }
 
 }
