3dd6a4f2214796942a7add6ee401c0adb87a007f
sundelun <76630582+sundelun@users.noreply.github.com>
2023-11-29 16:02:40 -0500

    Update CheckPOStFragment.java

--- app/src/main/java/com/example/b07_project/ui/checkpost/CheckPOStFragment.java
+++ app/src/main/java/com/example/b07_project/ui/checkpost/CheckPOStFragment.java
@@ -25,7 +25,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public class CheckPostFragment extends Fragment {
+public class CheckPOStFragment extends Fragment {
     /*
         //private FragmentPostBinding binding;
 //
