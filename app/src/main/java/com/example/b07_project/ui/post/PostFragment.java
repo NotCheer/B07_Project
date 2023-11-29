99c0d05b44404ca474ad3550237e6f32c1ad0b3b
MegumiKat <2243997366@qq.com>
2023-11-29 16:13:46 -0500

    admin post

--- app/src/main/java/com/example/b07_project/ui/post/PostFragment.java
+++ app/src/main/java/com/example/b07_project/ui/post/PostFragment.java
@@ -1,5 +1,6 @@
 package com.example.b07_project.ui.post;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,7 +19,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.example.b07_project.databinding.FragmentPostannBinding;
+import com.example.b07_project.databinding.FragmentPostannouncementBinding;
 import com.google.firebase.database.ValueEventListener;
 
 public class PostFragment extends Fragment {
@@ -33,11 +34,12 @@ public class PostFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_postann, container, false);
+        View view = inflater.inflate(R.layout.fragment_postannouncement, container, false);
 
         // Initialize views
         editTextTitle = view.findViewById(R.id.text_title);
